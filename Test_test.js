8a6a282686995e47e3839a5d110ef39190788507
IPP2015 <panduputra.ilham@gmail.com>
2021-11-28 05:34:08 +0700

    9

--- Test_test.js
+++ Test_test.js
@@ -1,16 +1,16 @@
 Feature('Create Account Cermati');
-// // Scenario('Create Account Sukses', ({ I }) => {
-// //     I.amOnPage('https://www.cermati.com/gabung-v2?')
-// //     I.fillField('email','panduputra.ilham@gmail.com')
-// //     I.fillField('password','Cermati123');
-// //     I.fillField('confirmPassword','Cermati123')
-// //     I.fillField('firstName','Ilham')
-// //     I.fillField('lastName','Putra')
-// //     I.fillField('mobilePhone','085777641438')
-// //     I.fillField('residenceCity','KOTA JAKARTA SELATAN')
-// //     I.click('.btn')
-// //     I.wait(2)
-// // });
+Scenario('Create Account Sukses', ({ I }) => {
+    I.amOnPage('https://www.cermati.com/gabung-v2?')
+    I.fillField('email', 'testing.2021.test.123@gmail.com')
+    I.fillField('password', 'Cermati123');
+    I.fillField('confirmPassword', 'Cermati123')
+    I.fillField('firstName', 'Ilham')
+    I.fillField('lastName', 'Putra')
+    I.fillField('mobilePhone', '085777641438')
+    I.fillField('residenceCity', 'KOTA JAKARTA SELATAN')
+    I.click('.btn')
+    I.wait(2)
+});
 Scenario('Check require Wajib Isi', ({ I }) => {
     I.amOnPage('https://www.cermati.com/gabung-v2?')
     I.click('.btn')
@@ -18,71 +18,71 @@ Scenario('Check require Wajib Isi', ({ I }) => {
 });
 Scenario('Check require Email', ({ I }) => {
     I.amOnPage('https://www.cermati.com/gabung-v2?')
-    I.fillField('email','panduputra. ilham@gmail.com')
+    I.fillField('email', 'panduputra. ilham@gmail.com')
     I.click('.text-header')
     I.see('Format email tidak valid.')
-    I.fillField('email','panduputra.ilhamgmail.com')
+    I.fillField('email', 'panduputra.ilhamgmail.com')
     I.click('.text-header')
     I.see('Format email tidak valid.')
     I.click('.text-header')
     I.see('Format email tidak valid.')
-    I.fillField('email','panduputra.ilham@gmail.')
+    I.fillField('email', 'panduputra.ilham@gmail.')
     I.click('.text-header')
     I.see('Format email tidak valid.')
-    I.fillField('email','panduputra.ilham@ gmail.co.id')
+    I.fillField('email', 'panduputra.ilham@ gmail.co.id')
     I.click('.text-header')
     I.see('Format email tidak valid.')
-    I.fillField('email','panduputra.ilham@gmail.co.id')
+    I.fillField('email', 'panduputra.ilham@gmail.co.id')
     I.click('.text-header')
     I.dontSee('Format email tidak valid.')
 });
 Scenario('Check require password', ({ I }) => {
-    var req2='Kata Sandi tidak sama dengan Konfirmasi Kata Sandi'
-    var req1='Kata Sandi harus memiliki panjang antara 8 - 50 karakter dan mengandung minimal 1 huruf besar, 1 huruf kecil, dan 1 angka.'
+    var req2 = 'Kata Sandi tidak sama dengan Konfirmasi Kata Sandi'
+    var req1 = 'Kata Sandi harus memiliki panjang antara 8 - 50 karakter dan mengandung minimal 1 huruf besar, 1 huruf kecil, dan 1 angka.'
     I.amOnPage('https://www.cermati.com/gabung-v2?')
-    I.fillField('password','CERMATI')
+    I.fillField('password', 'CERMATI')
     I.click('.text-header')
     I.see(req1)
-    I.fillField('password','cermat1')
+    I.fillField('password', 'cermat1')
     I.click('.text-header')
     I.see(req1)
-    I.fillField('password','Cermat2')
+    I.fillField('password', 'Cermat2')
     I.click('.text-header')
     I.see(req1)
-    I.fillField('password','CERmai3')
+    I.fillField('password', 'CERmai3')
     I.click('.text-header')
     I.see(req1)
-    I.fillField('password','CERmati5')
+    I.fillField('password', 'CERmati5')
     I.click('.text-header')
     I.click('.fa-eye-slash')
     I.dontSee(req1)
-    I.fillField('confirmPassword','CERmai5')
+    I.fillField('confirmPassword', 'CERmai5')
     I.click('.text-header')
     I.see(req2)
-    I.fillField('confirmPassword','CERmati5')
+    I.fillField('confirmPassword', 'CERmati5')
     I.click('.text-header')
     I.click('.fa-eye-slash')
     I.dontSee(req2)
 });
 Scenario('Check require Nama', ({ I }) => {
-    var req1='Input ini wajib diisi.'
-    let req2="Nama hanya dapat diisi dengan karakter alfabet, titik (.), petik ('), koma (,), strip (-) dan spasi."
+    var req1 = 'Input ini wajib diisi.'
+    let req2 = "Nama hanya dapat diisi dengan karakter alfabet, titik (.), petik ('), koma (,), strip (-) dan spasi."
     I.amOnPage('https://www.cermati.com/gabung-v2?')
-    I.fillField('email','panduputra.ilham@gmail.co.id')
-    I.fillField('password','cehDiide2')
-    I.fillField('confirmPassword','cehDiide2')
+    I.fillField('email', 'panduputra.ilham@gmail.co.id')
+    I.fillField('password', 'cehDiide2')
+    I.fillField('confirmPassword', 'cehDiide2')
     I.click('.btn')
     I.click('.text-header')
     I.see(req1)
-    I.fillField('firstName','CERMATI1')
+    I.fillField('firstName', 'CERMATI1')
     I.click('.text-header')
     I.see(req2)
-    I.fillField('firstName',"C.', -")
+    I.fillField('firstName', "C.', -")
     I.click('.text-header')
-    I.fillField('lastName','CERMATI1')
+    I.fillField('lastName', 'CERMATI1')
     I.click('.text-header')
     I.see(req2)
-    I.fillField('lastName',"C.', -")
+    I.fillField('lastName', "C.', -")
     I.click('.text-header')
     I.see(req1)
     I.dontSee(req2)
@@ -124,7 +124,7 @@ Scenario('Check No Telpon', ({ I }) => {
     I.see('Maaf, akun Anda sudah terdaftar.')
     I.wait(10)
 })
-Scenario('syarat dan kententuan', ({I}) => {
+Scenario('syarat dan kententuan', ({ I }) => {
     I.amOnPage('https://www.cermati.com/gabung-v2?')
     I.click("a[href='/pages/terms-and-conditions']")
     I.see('Syarat dan Ketentuan')
